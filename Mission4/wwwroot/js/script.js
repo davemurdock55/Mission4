ad5b719f32439e806e3830843bd56d09013601a9
davemurdock55 <davemurdock55@gmail.com>
2023-01-30 15:15:23 -0700

    Started the scripts.js file

--- Mission4/wwwroot/js/script.js
+++ Mission4/wwwroot/js/script.js
@@ -1,1 +1,23 @@
-﻿
+﻿/*
+     A "Grade Calculator" form that takes as input a percentage scored for each grade
+        category listed in the syllabus
+
+
+        Build a JavaScript program that calculates and displays both a final percentage and a letter grade
+        using the "Grade Calculator" form entries based on the weights listed in the syllabus. Use
+        jQuery selectors (i.e. the “$”) in your program. Print the letter grade to the form (not in an alert.)
+
+
+        Write good code (use good indentation, commenting, whitespace, descriptive variable names,
+        etc.) Good style counts for 10% of the score on each assignment. -->
+        <!-- Grade weights (approximate):
+            Assignments 50 %
+            Group Project 10 %
+            Quizzes 10 %
+            Midterm Exam 10 %
+            Final Exam 10 %
+            INTEX 10 %
+*/
+
+// Your code goes here
+let $('#AssignmentsInput')
